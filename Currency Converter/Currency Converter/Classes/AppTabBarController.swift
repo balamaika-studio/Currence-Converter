4575a0118929e7b8bbd38e79b6ae50da99e7aa57
Kirill <dev.kirill.klimenkov@gmail.com>
2020-05-24 14:15:19 +0300

    Implemented tab bar swipe transition

--- Currency Converter/Currency Converter/Classes/AppTabBarController.swift
+++ Currency Converter/Currency Converter/Classes/AppTabBarController.swift
@@ -11,10 +11,57 @@ import UIKit
 class AppTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegate = self
+        setupSwipeGesture()
         setUpTheming()
+    }
+    
+    private func setupSwipeGesture() {
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        view.addGestureRecognizer(swipeRight)
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        view.addGestureRecognizer(swipeLeft)
+    }
+    
+    @objc private func swiped(_ gesture: UISwipeGestureRecognizer) {
+        guard let controllers = viewControllers else {
+            return
+        }
+        var offset = 0
+        var newIndex = selectedIndex
+        let maxIndex = controllers.count - 1
+        
+        switch gesture.direction {
+        case .left:
+            offset = selectedIndex < maxIndex ? 1 : 0
+        case .right:
+            offset = selectedIndex > 0 ? -1 : 0
+        default: break
+        }
+        
+        newIndex += offset
+        let newViewController = controllers[newIndex]
+        if tabBarController(self, shouldSelect: newViewController) {
+            selectedIndex = newIndex
+        }
     }
 }
 
+// MARK: - UITabBarControllerDelegate
+extension AppTabBarController: UITabBarControllerDelegate  {
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SwipeTabBarTransition(viewControllers: tabBarController.viewControllers)
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        return true
+    }
+}
+
+// MARK: - UITabBarControllerDelegate
 extension AppTabBarController: Themed {
     func applyTheme(_ theme: AppTheme) {
         tabBar.barTintColor = theme.barBackgroundColor
